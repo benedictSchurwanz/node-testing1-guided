91e9219b2d24ab4cc4447ae306009f7d80df6420
Ladrillo <gabriel@cabrejas.net>
2021-12-15 18:30:29 +0100

    committing to "lecture"

--- car/car.spec.js
+++ car/car.spec.js
@@ -3,11 +3,10 @@ const { giveTrue } = require('./car')
 describe('learning unit testing', () => {
   // tests
   it('works fine', () => { // test
-    // assertions
     const actual = giveTrue()
     const expected = true
 
     expect(actual).toBe(expected) // assertion
-    expect(giveTrue()).toBe(true)
+    expect(giveTrue()).toBe(true) // assertion
   })
 })
