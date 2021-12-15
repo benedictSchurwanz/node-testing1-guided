1987024ab6739a202b5fd2f1ee5e0060d121f2ad
Ladrillo <gabriel@cabrejas.net>
2021-12-15 18:26:10 +0100

    committing to "lecture"

--- car/car.spec.js
+++ car/car.spec.js
@@ -8,3 +8,13 @@ describe('learning unit testing', () => {
     expect(actual).not.toBe(false) // another assertion
   })
 })
+describe('learning unit testing 2', () => {
+  // tests
+  it('works fine', () => { // test
+    // assertions
+    const expected = true
+    const actual = true
+    expect(actual).toBe(expected) // assertion
+    expect(actual).not.toBe(false) // another assertion
+  })
+})
